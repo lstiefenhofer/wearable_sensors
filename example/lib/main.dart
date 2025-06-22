ddf144fcd5006173ace9f1ba74000d62a12a64a1
Leonie Stiefenhofer <leonie.stiefenhofer@student.uni-tuebingen.de>
2025-06-22 23:32:44 +0200

    added gyro and accelerometer stream

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,17 +19,32 @@ class _MyAppState extends State<MyApp> {
 
   final _wearableSensorsPlugin = WearableSensors();
 
+  Stream<Map<String, double>> _gyroStream = Stream.empty();
+  Stream<Map<String, double>> _acceStream = Stream.empty();
+
   @override
   void initState() {
     super.initState();
     initPlatformState();
-    initStream("gyroscope");
+    initStreams();
+  }
+
+
+  void initStreams() async {
+    final gyroStream = await _wearableSensorsPlugin.createSensorStream("gyroscope");
+    final acceStream = await _wearableSensorsPlugin.createSensorStream("accelerometer");
+
+    if (! mounted) return;
+
+    setState(() {
+      _gyroStream = gyroStream;
+      _acceStream = acceStream;
+    });
   }
 
   String _platformVersion = 'Unknown';
 
-  Stream<Map<String, double>> _gyroStream = Stream.empty();
-
+  
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
@@ -49,26 +64,26 @@ class _MyAppState extends State<MyApp> {
   }
 
     // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initStream(String requestedStream) async {
-    Stream<Map<String, double>> gyroStream;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      gyroStream =
-          _wearableSensorsPlugin.createSensorStream(requestedStream);
-    } on PlatformException {
-      gyroStream = Stream.empty();
-    }
+  // Future<void> initStream(String requestedName, Stream flutterStream) async {
+  //   Stream<Map<String, double>> sensorStream;
+  //   // Platform messages may fail, so we use a try/catch PlatformException.
+  //   // We also handle the message potentially returning null.
+  //   try {
+  //     sensorStream =
+  //         _wearableSensorsPlugin.createSensorStream(requestedName);
+  //   } on PlatformException {
+  //     sensorStream = Stream.empty();
+  //   }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+  //   // If the widget was removed from the tree while the asynchronous platform
+  //   // message was in flight, we want to discard the reply rather than calling
+  //   // setState to update our non-existent appearance.
+  //   if (!mounted) return;
 
-    setState(() {
-      _gyroStream = gyroStream;
-    });
-  }
+  //   setState(() {
+  //     flutterStream = sensorStream;
+  //   });
+  // }
 
 
   @override
@@ -79,11 +94,11 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: ListView(
             children: [
-              Text('hello: $_platformVersion'),
-              SensorStreamBuilder(stream: _gyroStream, streamTitle: "gyroscope",),
+              Text('hi: $_platformVersion'),
+              Center(child: SensorStreamBuilder(stream: _gyroStream, streamTitle: "gyroscope",)),
+              Center(child: SensorStreamBuilder(stream: _acceStream, streamTitle: "accelerometer",)),
             ],
           ),
         ),
