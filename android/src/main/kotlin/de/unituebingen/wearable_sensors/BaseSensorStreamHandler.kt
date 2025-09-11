fcd6b3b1d19d53dbb87196fc74aad82e7c5a4827
Leonie Stiefenhofer <leonie.stiefenhofer@student.uni-tuebingen.de>
2025-09-11 16:28:32 +0200

    changed sensor delay to GAME

--- android/src/main/kotlin/de/unituebingen/wearable_sensors/BaseSensorStreamHandler.kt
+++ android/src/main/kotlin/de/unituebingen/wearable_sensors/BaseSensorStreamHandler.kt
@@ -21,7 +21,7 @@ abstract class BaseSensorStreamHandler(
             return
         }
         this.eventSink = sink
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME)
     }
 
     override fun onCancel(arguments: Any?) { 
